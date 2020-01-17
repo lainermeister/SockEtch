f2ff2cab39baa8d6dc851b754c3744e72bc378ec
Elaine <eyw2008@gmail.com>
2020-01-17 18:40:55 -0500

    final changes

--- db/helpers.js
+++ db/helpers.js
@@ -8,18 +8,3 @@ module.exports.getRandomString = (n) =>
         alphabet[module.exports.getRandomNumber(alphabet.length)]
     ).join("")
 
-
-
-// module.exports = {
-//     getRandomWord: () => {
-//         const startChar = alphabet[getRandomNumber(alphabet.length)]
-//         return axios.get(`https://api.datamuse.com/words?sp=${startChar}*&md=psf`)
-//             .then(({ data }) => {
-//                 let randomWord = data[getRandomNumber(data.length)]
-//                 while (!randomWord.tags.includes("n")) {
-//                     randomWord = data[getRandomNumber(data.length)]
-//                 }
-//                 return randomWord.word.toLowerCase();
-//             })
-//     }
-// }
